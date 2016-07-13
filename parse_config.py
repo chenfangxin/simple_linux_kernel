20bc19a870e5c4cffd048379039d824c3779334e
chenfangxin <derney@163.com>
2016-07-14 01:51:51 +0800

    no comments

--- parse_config.py
+++ parse_config.py
@@ -3,15 +3,19 @@
 import os
 import re
 
-dst_file = './linux-4.4.10/.config'
-configs = []
-with open(dst_file) as f:
-	for line in f:
-		line=line.strip()
-		if line:
-			if not re.match('^#', line):
-				item = line.split('=')[0]
-				configs.append(item)
+def parse_config(path):
+	configs = []
+	dst_file = path + '.config'
+	with open(dst_file) as f:
+		for line in f:
+			line=line.strip()
+			if line:
+				if not re.match('^#', line):
+					item = line.split('=')[0]
+					configs.append(item)
+	return configs
 
-print "CFG item num :" + str(len(configs))
-print configs
+if __name__=='__main__':
+	configs = parse_config('./linux-4.4.10/')
+#	print "CFG item num :" + str(len(configs))
+	print configs
